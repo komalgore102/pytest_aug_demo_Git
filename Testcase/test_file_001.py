08391b9840f58d5bd0f323426e72e78084a5b5be
komalgore102 <komal3695@gmail.com>
2023-08-23 16:15:14 +0530

    commitd

--- Testcase/test_file_001.py
+++ Testcase/test_file_001.py
@@ -1,21 +1,21 @@
-from selenium import webdriver
-
-
+#from selenium import webdriver
 #
+#
+# #
 class Test_001:
-    def test_Credence_001(self):
-        driver=webdriver.Chrome()
-        driver.get("https://credence.in/")
-        if driver.title == 'Credence':
-            driver.save_screenshot("D:\\Python seleinium\\Pytest_aug_demo\\Screenshots\\Credence.PNG")
-            assert  True
+     # def test_Credence_001(self):
+     #     driver=webdriver.Chrome()
+     #     driver.get("https://credence.in/")
+     #     if driver.title == 'Credence':
+     #         driver.save_screenshot("D:\\Python seleinium\\Pytest_aug_demo\\Screenshots\\Credence.PNG")
+     #         assert  True
+     #
+     #     else:
+     #        print("You are at wrong place")
+     #        driver.close()
+     #        assert False
 
-        else:
-            print("You are at wrong place")
-            driver.close()
-            assert False
-
-    def test_sum_002(self):
+     def test_sum_002(self):
         a=30
         b=50
         sum=a+b
